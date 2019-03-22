e86dd46765f46d00d7c493cee9dfe22c884d105a
JiaSyuan Li <jiasyuan@hotmail.com>
2019-03-22 17:13:25 +0800

    Add CustomersController

---
+++ Northwind/Northwind.Service/Controllers/_Customers/CustomersController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Northwind.Contract.Model;
+using Northwind.Repository.;
+
+namespace Northwind.Service.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CustomersController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<Customer> GetAllCustomers()
+        {
+            return null;
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
