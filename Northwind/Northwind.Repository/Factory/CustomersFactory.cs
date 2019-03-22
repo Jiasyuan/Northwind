cf8c45080142c71a84d3524048de1bcc4f241430
JiaSyuan Li <jiasyuan@hotmail.com>
2019-03-22 17:28:36 +0800

    Modify Factory

--- Northwind/Northwind.Repository/Factory/CustomersFactory.cs
+++ Northwind/Northwind.Repository/Factory/CustomersFactory.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Northwind.Repository.Common.Helper;
+using Northwind.Repository.Repository;
 
 namespace Northwind.Repository.Factory
 {
     public static class CustomersFactory
     {
+        private static CustomersRepository _customersRepository => new CustomersRepository(new NorthwindDbConnectionHelper());
+        public static CustomersRepository CustomersRepository = _customersRepository;
     }
 }
