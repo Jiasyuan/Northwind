8a595e9232447f7798741e0723436ef516a2d281
JiaSyuan <jiasyuan@hotmail.com>
2019-03-22 20:50:51 +0800

    Add CustomersApiUnitTest

---
+++ Northwind/Northwind.UnitTest/Service/CustomersApiUnitTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.UnitTest.Service
+{
+    [TestFixture()]
+    public class CustomersApiUnitTest
+    {
+        [Test]
+        public void GetTestGetAllCustomersApi()
+        {
+
+        }
+    }
+}
