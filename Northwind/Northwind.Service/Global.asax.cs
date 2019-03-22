db947e2d72b61e95fe5bf70205df6d9bb066cb2d
JiaSyuan Li <jiasyuan@hotmail.com>
2019-03-22 16:09:26 +0800

    Setting Api output JSON

--- Northwind/Northwind.Service/Global.asax.cs
+++ Northwind/Northwind.Service/Global.asax.cs
@@ -18,6 +18,8 @@ namespace Northwind.Service
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            
+            GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
         }
     }
 }
